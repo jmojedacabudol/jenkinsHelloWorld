10068049aca91677bbfa209b13187f5974cd802d
jmojedacabudol <59623465+jmojedacabudol@users.noreply.github.com>
2021-11-12 18:31:46 +0800

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 class HelloWorld {
     public static void main(String args[]) {
-        System.out.println("Some changes made version 2");
+        System.out.println("trying webhooks");
     }
 }
