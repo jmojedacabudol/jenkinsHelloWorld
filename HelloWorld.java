e33aa172e653ed42467dadff38f1918ad50048cd
jmojedacabudol <jmojedacabudol@gmail.com>
2021-11-12 18:06:21 +0800

    second commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 class HelloWorld {
     public static void main(String args[]) {
-        System.out.println("Some changes made");
+        System.out.println("Some changes made version 2");
     }
 }
